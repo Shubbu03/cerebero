f98d0a1e37f41059cfac0d2576ab2a2dc6a42ceb
Shubbu03 <thatcoderguyshubham@gmail.com>
2025-03-12 07:49:26 +0530

    updated layout with session provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { SessionProvider } from "next-auth/react";
+import { SessionProvider } from "next-auth/react"; 
 
 export const metadata: Metadata = {
   title: "Create Next App",
