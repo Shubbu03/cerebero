568359fc600ad684011bd8f22b193f907123c862
Shubbu03 <thatcoderguyshubham@gmail.com>
2025-03-23 15:12:37 +0530

    added login page

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import AuthForm from "@/components/auth/AuthForm";
+
+export default async function LoginPage() {
+  const session = await getServerSession();
+
+  if (session) {
+    redirect("/dashboard");
+  }
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+      <AuthForm mode="login" />
+    </div>
+  );
+}
