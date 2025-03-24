fabbafa992c5c573a8b3ca69b6d0138ff1ecb4e6
Shubbu03 <thatcoderguyshubham@gmail.com>
2025-03-24 07:40:46 +0530

    updated signup form

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -10,9 +10,5 @@ export default async function SignUp() {
     redirect("/dashboard");
   }
 
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
-      <AuthForm mode="signup" />
-    </div>
-  );
+  return <AuthForm mode="signup" />;
 }
