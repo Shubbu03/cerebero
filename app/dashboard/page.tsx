b3e4e2d3ad434ba80b17f69af40b202df5bb7942
Shubbu03 <thatcoderguyshubham@gmail.com>
2025-03-25 07:47:35 +0530

    ver0 dashboard setup

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { TexturedBackground } from "@/components/background/TexturedBackground";
+
+export default function Dashboard() {
+  return (
+    <>
+      <TexturedBackground className="min-h-screen" dotPattern>
+        <h2 className="text-white">dashboard</h2>
+      </TexturedBackground>
+    </>
+  );
+}
