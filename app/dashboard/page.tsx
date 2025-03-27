b1e050363d3b348d4ae1ead971373d6b7aa8f9ba
Shubbu03 <thatcoderguyshubham@gmail.com>
2025-03-27 08:07:17 +0530

    initial setup dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,14 @@
 import { TexturedBackground } from "@/components/background/TexturedBackground";
+import { FloatingDock } from "@/components/FloatingDock";
 
 export default function Dashboard() {
   return (
     <>
       <TexturedBackground className="min-h-screen" dotPattern>
         <h2 className="text-white">dashboard</h2>
+        <div className="fixed bottom-4 left-1/2 -translate-x-1/2 z-10">
+          <FloatingDock />
+        </div>
       </TexturedBackground>
     </>
   );
