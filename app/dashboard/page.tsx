701c3c2c64be2fabe9249e1758dc4b6d55ba6a1a
Shubbu03 <thatcoderguyshubham@gmail.com>
2025-03-27 22:20:53 +0530

    minor ui fix in dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,9 +6,7 @@ export default function Dashboard() {
     <>
       <TexturedBackground className="min-h-screen" dotPattern>
         <h2 className="text-white">dashboard</h2>
-        <div className="fixed bottom-4 left-1/2 -translate-x-1/2 z-10">
-          <FloatingDock />
-        </div>
+        <FloatingDock />
       </TexturedBackground>
     </>
   );
