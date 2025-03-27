957230cc7a62b6a1420af8bf28961440a61ac2ed
Shubbu03 <thatcoderguyshubham@gmail.com>
2025-03-27 21:56:13 +0530

    open links in new tab

--- components/ui/floating-dock.tsx
+++ components/ui/floating-dock.tsx
@@ -167,7 +167,7 @@ function IconContainer({
   const [hovered, setHovered] = useState(false);
 
   return (
-    <Link href={href}>
+    <Link href={href} target="_blank" rel="noopener noreferrer">
       <motion.div
         ref={ref}
         style={{ width, height }}
