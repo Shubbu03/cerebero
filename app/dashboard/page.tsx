2d1ad5ac7412d138556042756574f7d785a44a74
Shubbu03 <thatcoderguyshubham@gmail.com>
2025-03-28 07:59:58 +0530

    minor change

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,21 @@
+"use client"
+
 import { TexturedBackground } from "@/components/background/TexturedBackground";
 import { FloatingDock } from "@/components/FloatingDock";
+import { useState } from "react";
+import { IconPlus } from "@tabler/icons-react";
+import { Button } from "@/components/ui/button";
 
 export default function Dashboard() {
+  const [modalOpen, setModalOpen] = useState(false);
   return (
     <>
       <TexturedBackground className="min-h-screen" dotPattern>
         <h2 className="text-white">dashboard</h2>
         <FloatingDock />
+        <Button className="fixed bottom-4 right-4 rounded-full w-12 h-12 p-0 bg-accent-foreground text-white cursor-pointer">
+          <IconPlus className="h-6 w-6" />
+        </Button>
       </TexturedBackground>
     </>
   );
