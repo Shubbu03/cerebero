60c51af529eb762ef9188e3cd42d94568d830735
Shubbu03 <thatcoderguyshubham@gmail.com>
2025-04-03 07:59:24 +0530

    added route to get content tags

---
+++ app/api/get-content-tags/route.ts
@@ -0,0 +1,64 @@
+import { supabaseAdmin } from "@/lib/supabaseAdmin";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+const ParamSchema = z.object({
+  contentId: z.string().uuid("Invalid content ID format"),
+});
+
+// type Tag = {
+//   id: string;
+//   name: string;
+// };
+
+export async function GET(request: NextRequest) {
+  try {
+    const searchParams = request.nextUrl.searchParams;
+    const contentID = searchParams.get("contentID");
+
+    const validatedParams = ParamSchema.safeParse({ contentID });
+
+    if (!validatedParams.success) {
+      return NextResponse.json(
+        { error: validatedParams.error.format() },
+        { status: 400 }
+      );
+    }
+
+    const { contentId } = validatedParams.data;
+
+    const { data, error } = await supabaseAdmin
+      .from("content_tags")
+      .select(
+        `
+          tag_id,
+          tags!inner (
+            id,
+            name
+          )
+        `
+      )
+      .eq("content_id", contentId);
+
+    if (error) {
+      console.error("Supabase query error:", error);
+      return NextResponse.json(
+        { error: "Failed to fetch content tags" },
+        { status: 500 }
+      );
+    }
+
+    // const tags : Tag[] = data?.map((item) => ({
+    //   id: item.tag_id,
+    //   name: item.tags.name,
+    // }));
+
+    return NextResponse.json({ data }, { status: 200 });
+  } catch (error) {
+    console.error("Error fetching content tags:", error);
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 }
+    );
+  }
+}
