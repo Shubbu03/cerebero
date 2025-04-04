439f0a4e1c235060b8fc4656e8a8fef8bbebec53
Shubbu03 <thatcoderguyshubham@gmail.com>
2025-04-04 22:09:53 +0530

    dynamic header to display greeting

---
+++ components/ui/DynamicHeader.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+interface DynamicHeaderProps {
+  userName: string;
+}
+
+const getGreeting = (username: string): string => {
+  const currentHour = new Date().getHours();
+  if (currentHour < 12) {
+    return `Good morning ${username}!`;
+  } else if (currentHour < 18) {
+    return `What's good ${username}?`;
+  } else {
+    return `How was your day, ${username}?`;
+  }
+};
+
+export function DynamicHeader({ userName }: DynamicHeaderProps) {
+  const [greeting, setGreeting] = useState<string>("");
+
+  useEffect(() => {
+    setGreeting(getGreeting(userName));
+  }, [userName]);
+
+  return (
+    <div className="mb-6 md:mb-8">
+      {" "}
+      <h2 className="text-2xl md:text-3xl font-semibold text-white font-sans">
+        {greeting}
+      </h2>
+    </div>
+  );
+}
