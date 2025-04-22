67699a932190adff26094c11b1895bf643886acb
Shubbu03 <thatcoderguyshubham@gmail.com>
2025-04-22 22:40:02 +0530

    integrated Providers and AppLayout into RootLayout for improved session management and layout structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import { Metadata } from "next";
 import "./globals.css";
 import { GeistSans } from "geist/font/sans";
+import { Providers } from "./providers";
+import { AppLayout } from "@/components/layouts/AppLayout";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/options";
 
 export const metadata: Metadata = {
   title: "Cerebero",
@@ -9,14 +13,20 @@ export const metadata: Metadata = {
     "second-brain, notes, knowledge-management, data-storage, personal-wiki, information-organization, digital-memory ,PKM",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerSession(authOptions);
+
   return (
     <html lang="en">
-      <body className={`${GeistSans} antialiased`}>{children}</body>
+      <body className={`${GeistSans.className} antialiased`}>
+        <Providers session={session}>
+          <AppLayout>{children}</AppLayout>
+        </Providers>
+      </body>
     </html>
   );
 }
